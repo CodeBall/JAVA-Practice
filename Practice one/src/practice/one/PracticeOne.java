bafdf8dd001b90b3279d6e8c50fca840724d0446
CodeBall <m@yanzl.net>
2015-10-25 23:18:51 +0800

    飞花的糖果

--- Practice one/src/practice/one/PracticeOne.java
+++ Practice one/src/practice/one/PracticeOne.java
@@ -13,12 +13,9 @@ import java.util.*;
 class shulie{
 
     int fn(int n){
-        int y = 3,fn1 = 1,fn2 = 0;
-        int fn0 = 0;
-        for(y = 3;y <= n;y++){
-            fn0 = 4 * fn1 - 5 * fn2;
-            fn2 = fn1;
-            fn1 = fn0;
+        int fn0 = 1;
+        for(int y = 1;y <= n;y++){
+            fn0 *= y;
         }
         return fn0;
     }
@@ -29,11 +26,15 @@ public class PracticeOne {
 		Scanner sc=new Scanner(System.in);
                 shulie sl;
                 sl = new shulie();
-                int N = sc.nextInt();
-                while(N > 0){
-                    int x = sc.nextInt();
-                    System.out.println(sl.fn(x));
-                    N--;
+                while(sc.hasNext()){
+                    int n = sc.nextInt();
+                    int m = sc.nextInt();
+                    int nj = sl.fn(n);
+                    int mj = sl.fn(m);
+                    int nmj = sl.fn(n-m);
+                    int fm = mj * nmj;
+                    int zu = nj / fm;
+                    System.out.println(zu);
                 }
 	}
 }
