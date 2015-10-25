86b39a1f7cf6f721b6535e8c2996625d009b91a4
CodeBall <m@yanzl.net>
2015-10-25 23:57:27 +0800

    最佳拟合直线

--- Practice one/src/practice/one/PracticeOne.java
+++ Practice one/src/practice/one/PracticeOne.java
@@ -11,13 +11,36 @@ package practice.one;
  */
 import java.util.*;
 class shulie{
-
-    int fn(int n){
-        int fn0 = 1;
-        for(int y = 1;y <= n;y++){
-            fn0 *= y;
+    int[] xi = new int[16];
+    int[] yi = new int[16];
+    void shuli(int x,int y,int i){
+        xi[i] = x;
+        yi[i] = y;
+    }
+    int hxy(int n){
+        int chengji = 0;
+        for(int i = 0;i < n;i++){
+            chengji += xi[i] * yi[i];
         }
-        return fn0;
+        return chengji;
+    }
+    int hx(int n){
+        int hex = 0;
+        for(int i = 0;i < n;i++)
+            hex += xi[i];
+        return hex;
+    }
+    int hy(int n){
+        int hey = 0;
+        for(int i = 0;i < n;i++)
+            hey += yi[i];
+        return hey;
+    }
+    int hx2(int n){
+        int hex2 = 0;
+        for(int i = 0;i < n;i++)
+            hex2 = xi[i] * xi[i] + hex2;
+        return hex2;
     }
 }
 public class PracticeOne {
@@ -26,15 +49,22 @@ public class PracticeOne {
 		Scanner sc=new Scanner(System.in);
                 shulie sl;
                 sl = new shulie();
-                while(sc.hasNext()){
-                    int n = sc.nextInt();
-                    int m = sc.nextInt();
-                    int nj = sl.fn(n);
-                    int mj = sl.fn(m);
-                    int nmj = sl.fn(n-m);
-                    int fm = mj * nmj;
-                    int zu = nj / fm;
-                    System.out.println(zu);
+                int n = sc.nextInt();
+                for(int i = 0;i < n;i++){
+                    int x = sc.nextInt();
+                    int y = sc.nextInt();
+                    sl.shuli(x,y,i);
                 }
+                int hxy = sl.hxy(n);
+                int hx = sl.hx(n);
+                int hy = sl.hy(n);
+                int hx2 = sl.hx2(n);
+                int fza = n * hxy - hx * hy;
+                int fm = n * hx2 - hx * hx;
+                int fzb = hy * hx2 - hx * hxy;
+                double a = (double)fza / (double)fm;
+                double b = (double)fzb / (double)fm;
+                System.out.printf("%.3f\n",a);
+                System.out.printf("%.3f\n",b);
 	}
 }
