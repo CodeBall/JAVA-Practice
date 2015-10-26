5994f38250b66cd806238e7d33e89221ca8bd596
CodeBall <m@yanzl.net>
2015-10-26 17:07:36 +0800

    多项式求和

--- Practice one/src/practice/one/PracticeOne.java
+++ Practice one/src/practice/one/PracticeOne.java
@@ -10,66 +10,42 @@ package practice.one;
  * @author xqq
  */
 import java.util.*;
-import java.math.*;
 class shulie{
-   int c,k,z,m;
-   void Rect(int length){
-       if(length < 0)
-           length = 0;
-       c = length;
-       k = length;
-       z = 4 * length;
-       m = length * length;
-   }
-   void Rect(int length,int width){
-       if(width < 0)
-           width = 0;
-       if(length < 0)
-           length = 0;
-       c = length;
-       k = width;
-       z = 2 * (length + width);
-       m = length * width;
-   }
+    double[] dxs = new double[301];
+    void qz(){
+        double f = 1.0;
+        for(int i = 2;i < 301;i++){
+            if(i % 2 == 0)
+                f = f - 1/(double)i;
+            else
+                f = f + 1/(double)i;
+            dxs[i] = f;
+        }
+    }
+    double fh(int n){
+        return dxs[n];
+    }
 }
 public class PracticeOne {
-
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
                 shulie sl;
                 sl = new shulie();
-                String st;
-               while(sc.hasNext()){
-                   st = sc.nextLine();
-                   char[] str = st.toCharArray();
-                   int flag = 0,num = 0,a = 0,length = 0,width = 0;
-                   for(int i = 0;i < str.length;i++){
-                       if(str[i] == '-'){
-                           flag = 1;
-                       }
-                       else if(str[i] == ' '){
-                           if(flag == 1)
-                               length = 0;
-                           else
-                               length = num;
-                           flag = 2;
-                           num = 0;
-                       }
-                       else{
-                           a = str[i] - '0';
-                           num = num * 10 + a;
-                       }
-                   }
-                   if(flag == 0)
-                       length = num;
-                   if(flag == 2){
-                       width = num;
-                   }
-                   if(width == 0)
-                   sl.Rect(length);
-                   else
-                       sl.Rect(length, width);
-                   System.out.println(sl.c + " " +sl.k + " " + sl.z + " " + sl.m);
-               }
+                sl.qz();
+                while(sc.hasNext()){
+                    int n = sc.nextInt();
+                    double num;
+                    if(n == 1)
+                        num = 1.00;
+                    else if(n == 0)
+                        num = 0;
+                    else if(n > 300)
+                    {
+                        num = 0.69;
+                    }
+                    else
+                        num = sl.fh(n);
+                    System.out.printf("%.2f\n",num);
+                }
 	}
 }
