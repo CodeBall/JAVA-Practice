995ffd6ad286c047407d1b1db3ed20d2e546124d
CodeBall <m@yanzl.net>
2015-10-29 15:30:18 +0800

    计算各种图形的周长（接口与多态）

--- Practice one/src/practice/one/PracticeOne.java
+++ Practice one/src/practice/one/PracticeOne.java
@@ -10,57 +10,107 @@ package practice.one;
  * @author xqq
  */
 import java.util.*;
-class Rect{
-    double l,h,z;
-    public void set(double a,double b,double c){
-        l = a;
-        h = b;
-        z = c;
+interface Shape{
+    public double length();
+}
+class Triangle implements Shape{
+    double a,b,c;
+    public void get(double a,double b,double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public int pd(){
+        if(a + b <= c || b + c <= a || a + c <= b)
+            return 0;
+        else
+            return 1;
     }
     public double length(){
-        return 2 * (l + h);
-    }
-    public double area(){
-        return l * h;
+        return a + b + c;
     }
 }
-class Cubic extends Rect{
-    public double area(){
-        return 2 * super.area() + 2 * h * z + 2 * l * z;
+class Rectangle implements Shape{
+    double l,w;
+    public void get(double l,double w){
+        this.l = l;
+        this.w = w;
     }
-    public double value(){
-        return l * h * z;
-    } 
-}
-class Pyramid extends Rect{
-    public double area(){
-        double s1 = l * Math.sqrt((1.0/4 * h * h + z * z));
-       // System.out.printf("one = %f,two = %f\n",z,Math.sqrt((1/4 * h * h + z * z)));
-        double s2 = h * Math.sqrt((1.0/4 * l * l + z * z));
-        //System.out.printf("s1 = %f,s2 = %f\n",s1,s2);
-        return s1 + s2 + super.area();
-    }
-    public double value(){
-        return super.area() * z / 3.0;
+    public double length(){
+        return 2 * (l + w);
     }
 }
+
+class Circle implements Shape{
+    double r,PI = 3.14;
+    public void get(double r){
+        this.r = r;
+    }
+    public double length(){
+        return 2 * PI * r;
+    }
+}
+
 public class PracticeOne {
 	public static void main(String[] args) {
-            Scanner cin = new Scanner(System.in);
-            double l,h,z;
-            Cubic cubic = new Cubic();
-            Pyramid py = new Pyramid();
-	    while(cin.hasNext()){
-	    	l = cin.nextDouble();
-                h = cin.nextDouble();
-                z = cin.nextDouble();
-                if(l <= 0 || h <= 0 || z <= 0)
-                    System.out.printf("0.00 0.00 0.00 0.00\n");
-                else{
-                    cubic.set(l, h, z);
-                    py.set(l, h, z);
-                    System.out.printf("%.2f %.2f %.2f %.2f\n",cubic.area(),cubic.value(),py.area(),py.value());
-                }
-	    }
+            Scanner sc = new Scanner(System.in);
+           Triangle Tr = new Triangle();
+           Rectangle Re = new Rectangle();
+           Circle Ci = new Circle();
+           double num;
+           while(sc.hasNext()){
+               String st = sc.nextLine();
+               char[] str = st.toCharArray();
+               
+               double a = 0,b = 0,c = 01.456;
+               num = 0;
+               int flag = 0,xiao = 0,fu = 0;
+               for(int i = 0;i < str.length;i++){
+                   if(str[i] == '.'){
+                       xiao = 1;
+                   }
+                   else if(str[i] == '-'){
+                       fu = 1;
+                       break;
+                   }
+                   else if(str[i] == ' '){
+                       xiao = 0;
+                       if(flag == 0)
+                           a = num;
+                       else if(flag == 1)
+                           b = num;
+                       flag++;
+                       num = 0;
+                   }
+                   else{
+                       int x = str[i] - '0';
+                       if(xiao == 0)
+                           num = num * 10 + x;
+                       else
+                       {
+                            num = num + x * Math.pow(0.1,xiao);
+                            xiao++;
+                       }
+                   }
+               }
+               if(fu == 1){
+                   System.out.printf("0.00\n");
+               }
+               else if(flag == 0){
+                   a = num;
+                   Ci.get(a);
+                   System.out.printf("%.2f\n", Ci.length());
+               }
+               else if(flag == 1){
+                   b = num;
+                   Re.get(a, b);
+                   System.out.printf("%.2f\n", Re.length());
+               }
+               else if(flag == 2){
+                   c = num;
+                   Tr.get(a, b, c);
+                    System.out.printf("%.2f\n", Tr.length());
+               }
+           }
 	}
 }
