43d71e722359504658e3bd09f24cc8a9138b2b15
CodeBall <m@yanzl.net>
2015-10-29 12:59:41 +0800

    Point类的构造函数

--- Practice one/src/practice/one/PracticeOne.java
+++ Practice one/src/practice/one/PracticeOne.java
@@ -10,56 +10,29 @@ package practice.one;
  * @author xqq
  */
 import java.util.*;
-class Complex{
-    int real,image;
-    public Complex(int s,int x){
-        real = s;
-        image = x;
-    }
-    public void add(int s,int x){
-        real += s;
-        image += x;
-    }
-    public void sub(int s,int x){
-        real -= s;
-        image -= x;
-    }
-    public void cheng(int s,int x){
-        int f = image * s + real * x;
-        real = real * s - image * x;
-        image = f;
-    }
-    public void show(){
-        if(real == 0 && image == 0)
-            System.out.println("0");
-        else if(real == 0)
-            System.out.println(""+image + "i");
-        else if(image == 0)
-            System.out.println(""+real);
-        else if(image < 0)
-            System.out.println(""+real + image + "i");
-        else
-            System.out.println(""+real + "+" + image + "i");
-    }
+class people{
+	private int x;
+	private int  y;
+	people(){
+		this.x = 0;
+		this.y = 0;
+	}
+	people(int x,int y){
+		this.x = x;
+		this.y = y;
+	}
+	public void printf(){
+		System.out.println("("+this.x+","+this.y+")");
+	}
+	
 }
 public class PracticeOne {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-                int x = sc.nextInt();
-                int y = sc.nextInt();
-               Complex complex = new Complex(x,y);
-               while(sc.hasNext()){
-                   x = sc.nextInt();
-                   y = sc.nextInt();
-                   int z = sc.nextInt();
-                   if(z == 1)
-                       complex.add(x, y);
-                   else if(z == 2)
-                       complex.sub(x, y);
-                   else if(z == 3)
-                       complex.cheng(x, y);
-                   complex.show();
-               }
-               complex.show();
+            Scanner cin = new Scanner(System.in);
+	    while(cin.hasNext()){
+	    	people p = new people(cin.nextInt(),cin.nextInt());
+	    	System.out.println("(0,0)");
+	    	p.printf();
+	    }
 	}
 }
