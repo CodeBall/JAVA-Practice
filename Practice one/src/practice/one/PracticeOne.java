2158f36aa95395b7f70c1d4fd9c226859bc3dffc
CodeBall <m@yanzl.net>
2015-11-02 17:06:46 +0800

    最大矩形面积

--- Practice one/src/practice/one/PracticeOne.java
+++ Practice one/src/practice/one/PracticeOne.java
@@ -10,42 +10,149 @@ package practice.one;
  * @author xqq
  */
 
-import java.util.*;
+ import java.util.Scanner;
 
-class juxing{
-    int x1,y1,x2,y2;
-    public juxing(int a,int b,int c,int d){
-        x1 = a;
-        x2 = c;
-        y1 = b;
-        y2 = d;
-    }
-    public int pd(int x,int y){
-        if(x > x1 && x < x2){
-            if(y > y1 && y < y2)
-                return 1;
-            else
-                return 0;
-        }
-        else
-            return 0;
-    }
+class Point {
+	private int x;
+	private int y;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Point(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
 }
-  class PracticeOne {  
-    public static void main(String[] args) {  
-        Scanner sc = new Scanner(System.in);
-         while(sc.hasNext()){
-             int t = sc.nextInt();
-             int num = 0;
-             juxing point = new juxing(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt());
-             for(int i = 0;i < t;i++){
-                 int x = sc.nextInt();
-                 int y = sc.nextInt();
-                 if(point.pd(x, y) == 1)
-                     num++;
-             }
-             System.out.println(num);
-         }
-    }  
-}  
+public class PracticeOne {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			Point p1 = new Point(0, 0);
+			int l = sc.nextInt();
+			int h = sc.nextInt();
+			Point p2 = new Point(l, h);
+
+			int pointsCount = sc.nextInt();
+			Point[] points = new Point[pointsCount + 2];
+
+			points[0] = p1; 
+			points[pointsCount + 1] = p2;
+
+			for (int j = 1; j <= pointsCount; j++) 
+			{
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+				points[j] = new Point(x, y);
+			}
+
+			int area1 = areaFromXDirection(points, l, h);
+
+			int area2 = areaFromYDirection(points, l, h);
+
+			System.out.println(Math.max(area1, area2));
+		}
+	}
+
+	private static int areaFromYDirection(Point[] points, int l, int h) {
+		for (int i = 0; i < points.length - 1; i++) {
+			int k = i;
+			for (int j = i + 1; j < points.length; j++) {
+				if (points[j].getY() < points[k].getY()) {
+					k = j;
+				}
+                                else if(points[j].getY() == points[k].getY() && points[j].getX() < points[k].getX())
+                                    k = j;
+			}
+			if (k != i) {
+				Point p = points[i];
+				points[i] = points[k];
+				points[k] = p;
+			}
+		}
+		int maxArea = 0;
+		for (int i = 0; i < points.length - 1; i++) {
+			int low = 0;
+			int high = l;
+			int maxY = h - points[i].getY();
+			for (int j = i + 1; j < points.length; j++) {
+				if (points[j].getX() <= high && points[j].getX() >= low) {
+					maxArea = Math.max(maxArea,(points[j].getY() - points[i].getY())* (high - low));
+					if (points[j].getX() == points[i].getX()) {
+						break;
+					}
+					if (points[j].getX() > points[i].getX()) {
+						high = points[j].getX();
+					} else {
+						low = points[j].getX();
+					}
+					if ((high - low) * maxY < maxArea) {
+						break;
+					}
+				}
+			}
+		}
+		return maxArea;
+
+	}
+
+	private static int areaFromXDirection(Point[] points, int l, int h) {
+		for (int i = 0; i < points.length - 1; i++) {
+			int k = i;
+			for (int j = i + 1; j < points.length; j++) {
+				if (points[j].getX() < points[k].getX()
+						|| (points[j].getX() == points[k].getX())
+						&& (points[j].getY() < points[k].getY())) {
+					k = j;
+				}
+			}
+			if (k != i) {
+				Point p = points[i];
+				points[i] = points[k];
+				points[k] = p;
+			}
+		}
+		int maxArea = 0;
+		for (int i = 0; i < points.length - 1; i++) {
+			int low = 0;
+			int high = h;
+			int maxX = l - points[i].getX();
+			for (int j = i + 1; j < points.length; j++) {
+				if (points[j].getY() <= high && points[j].getY() >= low) {
+					maxArea = Math.max(maxArea,
+							(points[j].getX() - points[i].getX())
+									* (high - low));
+					if (points[j].getY() == points[i].getY()) {
+						break;
+					}
+					if (points[j].getY() > points[i].getY()) {
+						high = points[j].getY();
+					} else {
+						low = points[j].getY();
+					}
+					if ((high - low) * maxX < maxArea) {
+						break;
+					}
+				}
+			}
+		}
+		return maxArea;
+	}
+}
+
 
