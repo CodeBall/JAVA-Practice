92d0fb4e534b69c1847c84297017f075beb79097
CodeBall <m@yanzl.net>
2015-11-03 23:44:42 +0800

    答答租车系统

--- Practice one/src/practice/one/PracticeOne.java
+++ Practice one/src/practice/one/PracticeOne.java
@@ -12,147 +12,90 @@ package practice.one;
 
  import java.util.Scanner;
 
-class Point {
-	private int x;
-	private int y;
 
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public Point(int x, int y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
+class Cars{
+    private int money;
+    int getMoney(){
+        return money;
+    }
+    protected void setMoney(int m){
+        money = m;
+    }
+}
+class keche extends Cars{
+    int persons;
+    void keche(int p,int m){
+        persons = p;
+        setMoney(m);
+    }
+    int getPerson(){
+        return persons;
+    }
+}
+class pika extends Cars{
+    private int persons;
+    private double weight;
+    void pi_ka(int p,double t,int m){
+        persons = p;
+        weight = t;
+        super.setMoney(m);
+    }
+    int getPerson(){
+        return persons;
+    }
+    double getWeight(){
+        return weight;
+    }
+}
+class huo extends Cars{
+    private double weight;
+    void huo_che(double t,int m){
+        weight = t;
+        super.setMoney(m);
+    }
+    double getWeight(){
+        return weight;
+    }
 }
 public class PracticeOne {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+                keche[] K = new keche[10];
+                K[1].keche(5,800);
+                K[2].keche(5,400);
+                K[3].keche(5,800);
+                K[4].keche(51,1300);
+                K[5].keche(55,1500);
+                pika[] P = new pika[10];
+                P[6].pi_ka(5,0.45,500);
+                P[7].pi_ka(5,2.0,450);
+                huo[] H = new huo[11];
+                H[8].huo_che(3,200);
+                H[9].huo_che(25,1500);
+                H[8].huo_che(35,2000);
 		int t = sc.nextInt();
-		for (int i = 0; i < t; i++) {
-			Point p1 = new Point(0, 0);
-			int l = sc.nextInt();
-			int h = sc.nextInt();
-			Point p2 = new Point(l, h);
-
-			int pointsCount = sc.nextInt();
-			Point[] points = new Point[pointsCount + 2];
-
-			points[0] = p1; 
-			points[pointsCount + 1] = p2;
-
-			for (int j = 1; j <= pointsCount; j++) 
-			{
-				int x = sc.nextInt();
-				int y = sc.nextInt();
-				points[j] = new Point(x, y);
-			}
-
-			int area1 = areaFromXDirection(points, l, h);
-
-			int area2 = areaFromYDirection(points, l, h);
-
-			System.out.println(Math.max(area1, area2));
-		}
-	}
-
-	private static int areaFromYDirection(Point[] points, int l, int h) {
-		for (int i = 0; i < points.length - 1; i++) {
-			int k = i;
-			for (int j = i + 1; j < points.length; j++) {
-				if (points[j].getY() < points[k].getY()) {
-					k = j;
-				}
-                                else if(points[j].getY() == points[k].getY() && points[j].getX() < points[k].getX())
-                                    k = j;
-			}
-			if (k != i) {
-				Point p = points[i];
-				points[i] = points[k];
-				points[k] = p;
-			}
-		}
-		int maxArea = 0;
-		for (int i = 0; i < points.length - 1; i++) {
-			int low = 0;
-			int high = l;
-			int maxY = h - points[i].getY();
-			for (int j = i + 1; j < points.length; j++) {
-				if (points[j].getX() <= high && points[j].getX() >= low) {
-					maxArea = Math.max(maxArea,(points[j].getY() - points[i].getY())* (high - low));
-					if (points[j].getX() == points[i].getX()) {
-						break;
-					}
-					if (points[j].getX() > points[i].getX()) {
-						high = points[j].getX();
-					} else {
-						low = points[j].getX();
-					}
-					if ((high - low) * maxY < maxArea) {
-						break;
-					}
-				}
-			}
-		}
-		return maxArea;
-
-	}
-
-	private static int areaFromXDirection(Point[] points, int l, int h) {
-		for (int i = 0; i < points.length - 1; i++) {
-			int k = i;
-			for (int j = i + 1; j < points.length; j++) {
-				if (points[j].getX() < points[k].getX()
-						|| (points[j].getX() == points[k].getX())
-						&& (points[j].getY() < points[k].getY())) {
-					k = j;
-				}
-			}
-			if (k != i) {
-				Point p = points[i];
-				points[i] = points[k];
-				points[k] = p;
-			}
-		}
-		int maxArea = 0;
-		for (int i = 0; i < points.length - 1; i++) {
-			int low = 0;
-			int high = h;
-			int maxX = l - points[i].getX();
-			for (int j = i + 1; j < points.length; j++) {
-				if (points[j].getY() <= high && points[j].getY() >= low) {
-					maxArea = Math.max(maxArea,
-							(points[j].getX() - points[i].getX())
-									* (high - low));
-					if (points[j].getY() == points[i].getY()) {
-						break;
-					}
-					if (points[j].getY() > points[i].getY()) {
-						high = points[j].getY();
-					} else {
-						low = points[j].getY();
-					}
-					if ((high - low) * maxX < maxArea) {
-						break;
-					}
-				}
-			}
-		}
-		return maxArea;
+		if(t == 1){
+                    int pe = 0,mon = 0;
+                    double wei = 0.0;
+                    int n = sc.nextInt();
+                    for(int i = 0;i < n;i++){
+                        int id = sc.nextInt();
+                        int day = sc.nextInt();
+                        if(id >= 1 && id <= 5){
+                            pe += day * K[id].getPerson();
+                            mon += day * K[id].getMoney();
+                        }
+                        else if(id >= 6 && id <= 7){
+                            pe += day * P[id].getPerson();
+                            wei += day * P[id].getWeight();
+                            mon += day * P[id].getMoney();
+                        }
+                        else if(id >= 8 && id <= 10){
+                            wei += day * H[id].getWeight();
+                            mon += day * H[id].getMoney();
+                        }
+                    }
+                    System.out.printf("%d %.2f %d",pe,mon,wei);
+                }
 	}
 }
-
-
