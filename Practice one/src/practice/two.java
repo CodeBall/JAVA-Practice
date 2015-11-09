56bf30e38ec4561630bd1b2e10c58b707eec7dac
CodeBall <m@yanzl.net>
2015-11-09 20:16:17 +0800

    Eddy的难题

--- Practice one/src/practice/two.java
+++ Practice one/src/practice/two.java
@@ -15,22 +15,35 @@ import java.text.DecimalFormat;
 public class two {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-                int t = sc.nextInt();
-                String xi = sc.nextLine();
-                for(int j = 0;j < t;j++){
-                    String ti = sc.nextLine();
-                    char[] jiyou = ti.toCharArray();
-                     if(ti.length()<5)
-                    System.out.println(jiyou);
-                     else{
-                         for(int i = 4;i < ti.length();i++){
-                              if(jiyou[i]=='y'&&jiyou[i-1]=='Z'&&jiyou[i-2]=='a'&&jiyou[i-3]=='r'&&jiyou[i-4]=='C')  
-                              {jiyou[i-4]='c';jiyou[i-3]='R';jiyou[i-2]='A';jiyou[i-1]='z';jiyou[i]='Y';  }
-                               if(jiyou[i]=='Y'&&jiyou[i-1]=='z'&&jiyou[i-2]=='a'&&jiyou[i-3]=='R'&&jiyou[i-4]=='c')  
-                               {jiyou[i-4]='C';jiyou[i-3]='r';jiyou[i-2]='A';jiyou[i-1]='Z';jiyou[i]='y';  }
-                         }
-                         System.out.println(jiyou);
-                     }
+                while(sc.hasNext()){
+                    String str1 = sc.nextLine();
+                    String str2 = sc.nextLine();
+                    char[] s1 = str1.toCharArray();
+                    char[] s2 = str2.toCharArray();
+                    int i = 0,j = 0,flag = 0,l1 = str1.length(),l2 = str2.length();
+                    if(l1 >= l1){
+                        for(i = 0;i < l1;i++){
+                            for(j = 0;j < l2;j++){
+                                int t = 0;
+                                if((i + j) >= l1)
+                                    t = (i + j) % l1;
+                                else
+                                    t = i + j;
+                                if(s1[t] == s2[j])continue;
+                                else break;
+                            }
+                            if(j == l2){
+                                flag = 1;
+                                break;
+                            }
+                        }
+                        if(flag == 1)
+                            System.out.println("yes");
+                        else
+                            System.out.println("no");
+                    }
+                    else
+                        System.out.println("no");
                 }
 	}
 }
