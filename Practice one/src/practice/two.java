fba0d54ec0dd26c4f22dedfd80b62851324e8b62
CodeBall <m@yanzl.net>
2015-11-09 21:07:26 +0800

    回文时间（1）

--- Practice one/src/practice/two.java
+++ Practice one/src/practice/two.java
@@ -16,34 +16,42 @@ public class two {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
                 while(sc.hasNext()){
-                    String str1 = sc.nextLine();
-                    String str2 = sc.nextLine();
-                    char[] s1 = str1.toCharArray();
-                    char[] s2 = str2.toCharArray();
-                    int i = 0,j = 0,flag = 0,l1 = str1.length(),l2 = str2.length();
-                    if(l1 >= l1){
-                        for(i = 0;i < l1;i++){
-                            for(j = 0;j < l2;j++){
-                                int t = 0;
-                                if((i + j) >= l1)
-                                    t = (i + j) % l1;
-                                else
-                                    t = i + j;
-                                if(s1[t] == s2[j])continue;
-                                else break;
-                            }
-                            if(j == l2){
-                                flag = 1;
+                   int[] hour = {0,1,2,3,4,5,10,11,12,13,14,15,20,21,22,23,24};
+                   int[] minute = {0,10,20,30,40,50,1,11,21,31,41,51,2,12,22,32,60};
+                   String str = sc.nextLine();
+                   char[] time = str.toCharArray();
+                   int h = 0,m = 0,status = 0;
+                   for(int i = 0;i < str.length();i++){
+                       if(time[i] == ':'){
+                           h = status;
+                           status = 0;
+                       }
+                       else if(time[i] >= '0' && time[i] <= '9'){
+                           status = status * 10 + (time[i] - '0');
+                       }
+                   }
+                   m = status;
+                   int i = 0;
+                   DecimalFormat df1=new DecimalFormat("00");
+                   if(h == 23 && m >= 32)
+                       System.out.println("00:00");
+                   else{
+                       String ans1 = null;String ans2 = null;
+                        for(i = 0;i < 16;i++){
+                            if(hour[i] >= h && h < hour[i+1]){
+                                if(m < minute[i]){
+                                    ans1 = df1.format(hour[i]);
+                                    ans2 = df1.format(minute[i]);
+                                }
+                                else{
+                                    ans1 = df1.format(hour[i+1]);
+                                    ans2 = df1.format(minute[i+1]);
+                                }
                                 break;
                             }
                         }
-                        if(flag == 1)
-                            System.out.println("yes");
-                        else
-                            System.out.println("no");
-                    }
-                    else
-                        System.out.println("no");
+                        System.out.println(ans1 + ":" + ans2);
+                   }
                 }
 	}
 }
