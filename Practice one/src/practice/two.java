f9535ffc0090e66785c3b061b3d7005a26410a10
CodeBall <m@yanzl.net>
2015-11-12 20:13:48 +0800

    加密术

--- Practice one/src/practice/two.java
+++ Practice one/src/practice/two.java
@@ -10,34 +10,35 @@ package practice;
  * @author xqq
  */
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 public class two {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
                 while(sc.hasNext()){
-                   String str = sc.nextLine();
-                   char[] time = str.toCharArray();
-                   int h = 0,m = 0,status = 0;
-                   h=(time[0]-'0')*10+(time[1]-'0');  
-                   m=(time[3]-'0')*10+(time[4]-'0');  
-                   int a=h/10;  
-                   int b=h%10;  
-                   if(a+b*10>m && a+b*10<59)  
-                        System.out.printf("%02d:%02d\n",h,a+b*10);  
-                   else  
-                    {  
-                        if(h==23)  
-                            System.out.printf("%02d:%02d\n",0,0);  
-                        else  
-                        {  
-                            while(!(h+1<23 &&(h+1)%10*10+(h+1)/10<60))  
+                   String ss = sc.nextLine();
+                   String s = " ";
+                   String[] arr = ss.split("\\s+");
+                   String S = arr[0];
+                   String T = arr[1];
+                   char[] str1 = S.toCharArray();
+                   char[] str = T.toCharArray();
+                   int l = T.length();
+                   int p = S.length();
+                   int m = 0;
+                   for(int h = 0;h < l;h++){
+                       if(str1[m] == str[h])  
+                       {  
+                            m++;  
+                            if(m == p)  
                             {  
-                                h+=1;  
+                                System.out.printf("Yes\n");  
+                                break;  
                             }  
-                            System.out.printf("%02d:%02d\n",h+1,(h+1)%10*10+(h+1)/10);  
                         }  
-                    }  
+                   }
+                   if(m < p){
+                       System.out.printf("No\n");  
+                   }
                 }
 	}
 }
