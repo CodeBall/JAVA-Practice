787a7a5aaef18f1c6b55545d6edf647153f0d31c
CodeBall <m@yanzl.net>
2015-11-15 13:22:02 +0800

    骄傲的代价

--- Practice one/src/practice/two.java
+++ Practice one/src/practice/two.java
@@ -10,35 +10,20 @@ package practice;
  * @author xqq
  */
 import java.util.Scanner;
+import java.math.*;
+import java.text.*;
 
 public class two {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-                while(sc.hasNext()){
-                   String ss = sc.nextLine();
-                   String s = " ";
-                   String[] arr = ss.split("\\s+");
-                   String S = arr[0];
-                   String T = arr[1];
-                   char[] str1 = S.toCharArray();
-                   char[] str = T.toCharArray();
-                   int l = T.length();
-                   int p = S.length();
-                   int m = 0;
-                   for(int h = 0;h < l;h++){
-                       if(str1[m] == str[h])  
-                       {  
-                            m++;  
-                            if(m == p)  
-                            {  
-                                System.out.printf("Yes\n");  
-                                break;  
-                            }  
-                        }  
-                   }
-                   if(m < p){
-                       System.out.printf("No\n");  
-                   }
-                }
+               int T = sc.nextInt();
+               BigInteger a,b;
+               for(int i = 1;i <= T;i++){
+                   a = sc.nextBigInteger();
+                   b = sc.nextBigInteger();
+                   System.out.println("Case " + i + ":\n" + a + " + " + b + " = " + a.add(b));
+                   if(i < T)
+                       System.out.println();
+               }
 	}
 }
