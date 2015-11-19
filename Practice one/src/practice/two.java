cca7d6fed6798f91a20f21a54e4f14492909e08e
CodeBall <m@yanzl.net>
2015-11-19 15:40:39 +0800

    字符串扩展

--- Practice one/src/practice/two.java
+++ Practice one/src/practice/two.java
@@ -12,42 +12,33 @@ package practice;
 import java.util.Scanner;
 import java.math.*;
 import java.text.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
 
 public class two {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-                while(sc.hasNext()){
-                    int t = sc.nextInt();
-                    List<Integer> list = new ArrayList<Integer>();
-                    for(int i = 0;i < t;i++){
-                        int in = sc.nextInt();
-                        list.add(in);
-                    }
-                    List<Integer> listTemp = new ArrayList<Integer>();
-                    Iterator<Integer> it=list.iterator();
-                    while(it.hasNext()){
-                        int a = it.next();
-                        if(listTemp.contains(a)){
-                            it.remove();
-                        }
-                        else{
-                            listTemp.add(a);
-                        }
-                    }
-                    Collections.sort(list);
-                    int len = list.size(),len1 = 0;
-                    for(Integer i:list){
-                        len1++;
-                        if(len1 < len)
-                        System.out.printf("%d ",i);
-                        else
-                            System.out.println(i);
+               int T = sc.nextInt();
+               for(int i = 0;i < T;i++){
+                   String str = sc.next();
+                   String newStr = temp(str);
+                   System.out.println(newStr);
+               }
+	}
+    public static String temp(String oldStr){
+        StringBuffer buffer = new StringBuffer();
+        char[] str = oldStr.toCharArray();
+        for(int i = 0;i < oldStr.length();i++){
+            buffer.append(str[i]);
+            if(str[i] == '-' && i > 0 && i < oldStr.length() - 1){
+                char st = str[i-1];
+                char ed = str[i+1];
+                if(st <= ed && ((st >= 'a' && ed <= 'z') || (st >= 'A' && ed <= 'Z') || (st >= '0' && ed <= '9'))){
+                    buffer.deleteCharAt(buffer.length() - 1);
+                    for(int j = st + 1;j <= ed - 1;j++){
+                        buffer.append((char) j);
                     }
                 }
-	}
+            }
+        }
+        return buffer.toString();
+    }
 }
-
